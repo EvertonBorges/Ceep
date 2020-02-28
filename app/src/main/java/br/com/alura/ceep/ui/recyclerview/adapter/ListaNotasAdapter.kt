cc41150f2724db710ecc024491bfaa0168c1a84f
EvertonBorges <evertonsoaresborges@gmail.com>
2020-02-28 10:40:37 -0400

    Aplicando refatoracao

--- app/src/main/java/br/com/alura/ceep/ui/recyclerview/adapter/ListaNotasAdapter.kt
+++ app/src/main/java/br/com/alura/ceep/ui/recyclerview/adapter/ListaNotasAdapter.kt
@@ -12,6 +12,7 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import br.com.alura.ceep.BR
 import br.com.alura.ceep.R
+import br.com.alura.ceep.databinding.ItemNotaBinding
 import br.com.alura.ceep.model.Nota
 import br.com.alura.ceep.ui.extensions.carregaImagem
 import kotlinx.android.synthetic.main.item_nota.view.*
@@ -23,8 +24,7 @@ class ListaNotasAdapter(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(context)
-        val viewDataBinding =
-            DataBindingUtil.inflate<ViewDataBinding>(inflater, R.layout.item_nota, parent, false)
+        val viewDataBinding = ItemNotaBinding.inflate(inflater, parent, false)
         return ViewHolder(viewDataBinding)
     }
 
@@ -34,7 +34,7 @@ class ListaNotasAdapter(
         }
     }
 
-    inner class ViewHolder(private val viewDataBinding: ViewDataBinding) :
+    inner class ViewHolder(private val viewDataBinding: ItemNotaBinding) :
         RecyclerView.ViewHolder(viewDataBinding.root) {
 
         private lateinit var nota: Nota
@@ -49,7 +49,7 @@ class ListaNotasAdapter(
 
         fun vincula(nota: Nota) {
             this.nota = nota
-            viewDataBinding.setVariable(BR.nota, nota)
+            viewDataBinding.nota = nota
         }
 
     }
@@ -64,9 +64,4 @@ object DiffCallback : DiffUtil.ItemCallback<Nota>() {
 
     override fun areContentsTheSame(oldItem: Nota, newItem: Nota) = oldItem == newItem
 
-}
-
-@BindingAdapter("carregaImagem")
-fun carregaImagemPorUrl(view: ImageView, url: String) {
-    view.carregaImagem(url)
 }
